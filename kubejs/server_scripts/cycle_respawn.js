ae7b79fdde3c4ef51ecbf6ae77b6e48db103e9bb
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-10-10 18:21:25 +0700

    0.1: fixed cycle respawn code error

--- kubejs/server_scripts/cycle_respawn.js
+++ kubejs/server_scripts/cycle_respawn.js
@@ -4,7 +4,7 @@ NativeEvents.onEvent("net.neoforged.neoforge.event.entity.player.PlayerRespawnPo
     if (!event.copyOriginalSpawnPosition() && event.entity.respawnDimension.path == $Level.OVERWORLD.path){
         let originalTransition = event.originalDimensionTransition;
         let _anchor_pos = event.entity.persistentData.get("anchor_pos");
-        let anchor_pos = {x: anchor_pos.getDouble("x"), y: anchor_pos.getDouble("y"), z: anchor_pos.getDouble("z")};
+        let anchor_pos = {x: _anchor_pos.getDouble("x"), y: _anchor_pos.getDouble("y"), z: _anchor_pos.getDouble("z")};
         event.setDimensionTransition({newLevel: event.entity.level, pos: new Vec3d(anchor_pos.x, anchor_pos.y, anchor_pos.z), speed: originalTransition.speed, yRot: originalTransition.yRot, xRot: originalTransition.xRot, missingRespawnBlock: !event.copyOriginalSpawnPosition, postDimensionTransition: entity => {}});
         event.setCopyOriginalSpawnPosition(true);
         event.entity.server.runCommandSilent(`execute in minecraft:overworld run spawnpoint ${event.entity.username} ${Math.round(anchor_pos.x)} ${Math.round(anchor_pos.y)} ${Math.round(anchor_pos.z)}`);
