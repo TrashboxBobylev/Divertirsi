cfd2d9db393b235f2af8ffcf54a090a5cc99799e
Trashbox Bobylev <trashbox.bobylev@gmail.com>
2025-12-18 15:25:08 +0700

    0.1: fixed duplicate code in tool_tier_slighthaul.js

--- kubejs/server_scripts/tool_tier_slighthaul.js
+++ kubejs/server_scripts/tool_tier_slighthaul.js
@@ -6,13 +6,6 @@ ServerEvents.tags("block", event => {
         }
     }
 
-    for (let tag of ["c:ores_in_ground/deepslate", "chipped:deepslate", "c:cobblestones/deepslate"]){
-        for (let block of event.get(tag).getObjectIds()){
-            event.remove("minecraft:needs_iron_tool", block);
-            event.add("minecraft:needs_diamond_tool", block);
-        }
-    }
-
     let steel = event.get("immersiveengineering:incorrect_for_steel_tool");
     steel.removeAll();
     steel.add("minecraft:incorrect_for_diamond_tool");
